67d8cd43131aac5878e5c24885a790178ffd05a8
agusb19 <agustinbrage19@gmail.com>
2024-06-23 00:58:39 -0300

    feat: add name attr to healthcare routes
    (avoid error on sideMenuItems reading)

--- frontend/src/api/healthcare/healthcare.routes.jsx
+++ frontend/src/api/healthcare/healthcare.routes.jsx
@@ -4,10 +4,12 @@ import { QueryViewerNoData } from '../../components/viewers/Queries.jsx';
 export const healthcareSubroutes = [
     {
         path: 'api',
+        name: 'api',
         element: <QueryViewerNoData useQueryHook={healthcareHooks.useHealthcareApi}/>
     },
     {
         path: 'database',
+        name: 'database',
         element: <QueryViewerNoData useQueryHook={healthcareHooks.useHealthcareDatabase}/>
     }
 ]
