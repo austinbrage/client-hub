f84e2872d4e2601e7808ccb72e06e0419566d6f5
agusb19 <agustinbrage19@gmail.com>
2024-06-23 00:59:54 -0300

    feat: return json with info data on healthcare server route

--- backend/server/routes/healthcare/healthcare.controller.js
+++ backend/server/routes/healthcare/healthcare.controller.js
@@ -1,4 +1,9 @@
+import { readFileSync } from "fs";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
+import { ENVIRONMENT } from "../../global/utils/config.js";
 import { CustomError } from "../../global/helpers/customError.js";
+import { createOkResponse } from '../../global/utils/responses.js';
 
 class HealthcareController {
     
@@ -6,17 +11,44 @@ class HealthcareController {
         this.pingPool = pingPool;
     }
 
-    checkApi = (_req, res) => {    
-        res.status(200).send('pong');
+    getApiData() {
+        const packagePath = join(dirname(fileURLToPath(import.meta.url)), '../../../package.json');
+        const packageFile = readFileSync(packagePath, 'utf-8');
+    
+        const packageJson = JSON.parse(packageFile);
+
+        return {
+            currentEnvironment: ENVIRONMENT,
+            apiName: packageJson?.name ?? 'no name available',
+            apiVersion: packageJson?.version ?? 'no version available'
+        }
+    }
+
+    checkApi = (_req, res) => {   
+        res.status(200).send(createOkResponse({
+            message: 'pong',
+            data: this.getApiData()
+        }));
     }
 
     checkDatabase = async (_req, res, next) => {
-        let connection
+        let connection;
+        const startTime = Date.now();
 
         try {
             connection = await this.pingPool.getConnection();
             await connection?.query('SELECT 1');
-            res.status(200).send('pong');
+            
+            const endTime = Date.now();
+            const connectionTime = endTime - startTime;
+
+            res.status(200).send(createOkResponse({
+                message: 'pong',
+                data: {
+                    ...this.getApiData(),
+                    connectionTime: connectionTime + ' ms'
+                }
+            }));
             
         } catch(err) {
             next(new CustomError(`Database connection error (${err})`));
